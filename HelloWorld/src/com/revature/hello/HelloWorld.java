f09a22a012757d2a5318d7c38411fa908e707605
genome3po <jeffrey.rubi.885@my.csun.edu>
2018-01-22 13:49:46 -0800

    Added HelloWorld.java

---
+++ HelloWorld/src/com/revature/hello/HelloWorld.java
@@ -0,0 +1,35 @@
+package com.revature.hello;
+/*
+ * package is namespace that organizes a set of related classes and interfaces
+ * corresponds to a folder structure
+ * base package for Java is java.lang
+ */
+
+public class HelloWorld {
+	
+	/*
+	 * Naming conventions:
+	 * classes and projects: pascal casing, FirstSecondThird
+	 * methods and variables: camel casing, firstSecondThird
+	 * package names: lowercase, separated by periods, e.g. com.revature.[other stuff]
+	 * constants: FIRST_SECOND_THIRD
+	 */
+	
+	/*
+	 * main method
+	 * JVM looks for a method with this signature as an entrance point to the program
+	 */
+	
+	public static void main(String[] args) {
+		//static: don't need an instance of HelloWorld to execute the method
+		//void: return type, doesn't return anything
+		//String[] args: array of string parameter to pass to main()
+		
+		//sysout+ctrl+space
+		System.out.println("Hello World!");
+		
+		//STS (and, under the hood, Eclipse) does incremental compiling
+		//every time a file is saved, it and any of its dependencies are compiled
+		//compiling: converting .java source file into .class bytecode for execution by JVM.
+	} // End main()
+} // End Class
