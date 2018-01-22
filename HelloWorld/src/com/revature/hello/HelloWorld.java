55fdd68faa236fafcdfdb887ed9354a2ea8ee490
Neil Bateman <batemangn@gmail.com>
2018-01-22 16:49:40 -0500

    The classic

---
+++ HelloWorld/src/com/revature/hello/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.revature.hello;
+/*
+ * A package is a namespace that organizes a set of related classes and interfaces.
+ * It corresponds to a folder structure.
+ * Java.lang is the base package of the Java language.
+ */
+
+public class HelloWorld {
+	/*
+	 * Naming Conventions:
+	 * Classes and Projects: Pascal Case - every first letter capitalized
+	 * Methods and Variables: Camel Case - after the first word, every first letter capitalized
+	 * Package: Lowercase, separated by periods
+	 * Constants: Uppercase, separated by underscores
+	 */
+	
+	/*
+	 * Main Method
+	 * The JVM looks for a method with this signature as an entry point to the program.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
