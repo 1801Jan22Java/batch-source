6511ef785c40689ab80cb28c846c8ca3a18b66b4
nrhassan <35673818+nrhassan@users.noreply.github.com>
2018-01-22 16:52:13 -0500

    adding helloworld

---
+++ HelloWorld/src/com/revature/hello/HelloWorld.java
@@ -0,0 +1,33 @@
+package com.revature.hello;
+/*
+ * a package is a namespace that organizes a set of related classes and interfaces
+ * corresponds to a folder structure
+ * base package for java is java.lang
+ */
+public class HelloWorld {
+	/*
+	 * Naming conventions
+	 * classes and project: pascal casing
+	 * variables and methods: camel casing
+	 * package names: lower case, separated by periods, 
+	 * constants: all caps 
+	 * 
+	 * 
+	 */
+
+	/*
+	 * main method 
+	 * JVM looks for a method with this signature as an entrance point to program
+	 * 
+	 */
+	public static void main(String[] args) {
+		//static: doesn't need an instance of HelloWorld to execute the method 
+		//void: return type
+		//String[] args: array of string parameter to pass to main
+		
+		System.out.println("Hello World");
+		//STS does incremental compiling 
+		//every time a file is saved, it and any of its dependencies are compiled
+		//compiling convertiong .java source files into .class bytecode
+	}
+}
