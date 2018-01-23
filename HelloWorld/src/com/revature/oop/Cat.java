78df634cf3226106f4dc358ee9e1a43589760543
erchiggins <emilyrchiggins@gmail.com>
2018-01-23 12:02:14 -0500

    tuesday morning

---
+++ HelloWorld/src/com/revature/oop/Cat.java
@@ -0,0 +1,32 @@
+package com.revature.oop;
+
+public class Cat extends Animal {
+	
+	public Cat(int numLives, String name) {
+		super();
+		super.setName(name);
+		this.numLives = numLives;
+	}
+
+	public static final String catinName = "Felis catus";
+	
+	private int numLives;
+
+	public int getNumLives() {
+		return numLives;
+	}
+
+	public void setNumLives(int numLives) {
+		this.numLives = numLives;
+	}
+	
+	public void makeNoise(){
+		System.out.println("meow");
+	}
+
+	@Override
+	public String toString() {
+		return "Cat [ name="+this.getName()+ "; numLives=" + numLives + "]";
+	}
+
+}
---
+++ HelloWorld/src/com/revature/oop/FunWithOop.java
@@ -0,0 +1,59 @@
+package com.revature.oop;
+
+public class FunWithOop {
+	
+	public static void main(String[] args){
+		
+		
+		//create a new Animal
+		Animal fluffy = new Animal();
+		System.out.println(fluffy.toString());
+		
+		//polymorphic instance method: toString
+		Object fluffy2 = new Animal(); //implicit upcasting
+		((Animal) fluffy2).setName("fluffy2"); 
+		//in order to access the Animal methods, we need to cast
+		//no implicit downcasting
+		//but! when we call fluffy2.toString, we get the Animal version
+		System.out.println(fluffy2.toString());
+		
+		//at compile time, the declared type is checked. 
+		//casts can work (at compile time) up or down the inheritance tree
+		Object fluffy3 = new Object();
+		//((Animal) fluffy3).setName("fluffy3");
+		//this throws a ClassCastException
+		//at runtime, the actual type is checked. 
+		
+		Cat c1 = new Cat(8,"Paul");
+		Cat c2 = new Cat(7,"Peter");
+		Dog d1 = new Dog();
+		
+		/*
+		 * Arrays:
+		 * Elements must be the same type, length must be specified
+		 * length is immutable (contiguous block of memory is allocated)
+		 * java.util.Arrays (note the s) for useful methods
+		 * all elements are set to default value for that data type 
+		 */
+		Animal[] animalArray = new Animal[3];
+		animalArray[0] = c1;
+		animalArray[1] = c2;
+		animalArray[2] = d1;
+		
+		for (int i = 0; i<animalArray.length; i++){
+			animalArray[i].makeNoise();
+		}
+		
+		//best practices: refer to static members via the class
+		System.out.println(Cat.catinName);
+		
+		Dog d2 = new Dog();
+		
+		//this will change the static field for ALL INSTANCES OF DOG
+		d2.latinName = "doggus doggus";
+		
+		System.out.println(d1.latinName);
+		System.out.println(d2.latinName);
+	}
+
+}
