4b20fd5105a4f8b73703481e47eaedd27fa7dd8b
sungkwonkudo <30812602+sungkwonkudo@users.noreply.github.com>
2018-01-24 11:34:49 -0500

    Q 10 done

--- Assignments/src/com/revature/homework1/Question10.java
+++ Assignments/src/com/revature/homework1/Question10.java
@@ -1,5 +1,13 @@
 package com.revature.homework1;
 
 public class Question10 {
-
+	public static void main(String[] args) {
+		System.out.println(min(1, 3));
+		System.out.println(min(10, 3));
+		
+	}
+	
+	public static int min(int a, int b) {
+		return a > b ? b : a;
+	}
 }
