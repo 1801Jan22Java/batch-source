b18acdac4ef9fb1bbe1d1a657da326f2a6640eef
sungkwonkudo <30812602+sungkwonkudo@users.noreply.github.com>
2018-01-23 20:39:25 -0500

    Q3 done

--- Assignments/src/com/revature/homework1/Question3.java
+++ Assignments/src/com/revature/homework1/Question3.java
@@ -1,5 +1,25 @@
 package com.revature.homework1;
 
 public class Question3 {
-
+	public static void main(String[] args) {
+		System.out.println(reverse("this is an input"));
+	}
+	
+	public static String reverse(String input) {
+		// An output variable
+		String output="";	
+		
+		// Start at the end of the string by beginning 
+		// from the total length of the input.
+		// Subtract from this value to get the index.
+		for(int i=input.length()-1; i>=0; i--) {
+			
+			// Get the character at index
+			// Convert to String using the Character static method toString()
+			// Concatenate to output
+			output=output.concat(Character.toString(input.charAt(i)));
+		}
+		
+		return output;
+	}
 }
