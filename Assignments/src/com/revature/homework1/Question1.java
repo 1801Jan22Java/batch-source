c053f8f0bae302d7b99f7f51864fb07c68367796
sungkwonkudo <30812602+sungkwonkudo@users.noreply.github.com>
2018-01-24 11:06:38 -0500

    Q2 done

--- Assignments/src/com/revature/homework1/Question1.java
+++ Assignments/src/com/revature/homework1/Question1.java
@@ -1,5 +1,32 @@
 package com.revature.homework1;
 
+// Sort the following using bubble sort: 1,0,5,6,3,2,3,7,9,8,4
 public class Question1 {
-
+	public static void main(String[] args) {
+		String strArray = "";
+		int[] arr = {1,0,5,6,3,2,3,7,9,8,4};
+		for(int n : bubbleSort(arr)) {
+			strArray += n + " ";
+		}
+		System.out.println("{ "+strArray+"}");
+	}
+	
+	public static int[] bubbleSort(int[] arr) {
+		// First loop to move the sorting window up
+		for(int left=0; left<arr.length; left++) {
+			
+			// Second loop to sort within the varying
+			// window sizes and locations
+			for(int right=0; right<left; right++) {
+				// If left element is less than right,
+				// swap array elements
+				if(arr[left]<arr[right]) {
+					int temp = arr[right];
+					arr[right]=arr[left];
+					arr[left]=temp;
+				}
+			}
+		}
+		return arr;
+	}
 }
--- Assignments/src/com/revature/homework1/Question4.java
+++ Assignments/src/com/revature/homework1/Question4.java
@@ -4,17 +4,36 @@ package com.revature.homework1;
 public class Question4 {
 	public static void main(String[] args) {
 		
-		// Testing with a small number
+		// Testing with 0
 		System.out.println(factorial(0));
 		
+		 //Testing with small numbers
+		System.out.println(factorial(3));
+		System.out.println(factorial(5));
+		System.out.println(factorial(7));
+		
+		// Testing with a negative number
+		System.out.println(factorial(-2));
+		
+		// Testing with char
+		System.out.println(factorial('q'));
 	}
 	
 	public static int factorial(int n) {
-		int answer=1;
-		for(int i=1; i<=n; i++) {
-			answer*=i;
+		try {
+			if(n<0) { 
+				throw new IllegalArgumentException();
+			}
+			int answer=1;
+			for(int i=1; i<=n; i++) {
+				answer*=i;
+			}
+			
+			return answer;
+		} catch(IllegalArgumentException e) {
+			System.out.println("N! of negative numbers not possible: "+e.toString());
+			return 0;
 		}
 		
-		return answer;
 	}
 }
