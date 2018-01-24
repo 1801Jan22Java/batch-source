49a0da6f60334ec5f18d3fe8828c8a437bb6bf0b
genome3po <jeffrey.rubi.885@my.csun.edu>
2018-01-24 10:49:16 -0800

    Mid Wednesday, 24Jan2018

---
+++ Assignments/src/com/revature/homework1/Question2.java
@@ -0,0 +1,14 @@
+package com.revature.homework1;
+
+/* Created by: Jeffrey Rubi
+ * Date: 24 January 2018
+ * Write a program to display the first 25 Fibonacci numbers beginning at 0.
+ */
+
+public class Question2 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ HelloWorld/src/com/revature/wednesday/Driver.java
@@ -0,0 +1,148 @@
+package com.revature.wednesday;
+
+import java.util.Arrays;
+
+public class Driver {
+
+	public static void main(String[] args) {
+
+		String[] members1 = {"Calder", "Kiera", "Chad"}; 
+		Batch b1 = new Batch("1801Jan22", "Java", members1);
+		
+		System.out.println(b1.hashCode());
+		
+		String[] members2 = {"Matt", "Conan", "Sungkwon"}; 
+		Batch b2 = new Batch("1801Jan22", "Java", members2);
+		
+		System.out.println(b2.hashCode());
+		
+		System.out.println(b1.equals(b2));
+		
+		//funWithArrays();
+		
+		/*
+		funWithVarArgs();
+		funWithVarArgs("cat");
+		funWithVarArgs("cat", "dog", "bird");
+		*/
+		
+		funWithStrings();
+	}
+	
+	public static void funWithStrings() {
+		
+		String a = "cow";
+		String b = new String("cow");
+		// what is this??
+		System.out.println(a == b);
+		// true
+		System.out.println(a.equals(b));
+		
+		// String API methods
+		
+		// indexOf
+		String str1 = "indexOfx";
+		System.out.println(str1.indexOf('x', 5));
+		
+		// isEmpty
+		String str2 = "empty";
+		if(str2.isEmpty()) {
+			System.out.println("string has length zero");
+		} else {
+			System.out.println("string has length " + str2.length());
+		}
+		
+		// format
+		String str3 = "hello";
+		System.out.println(String.format("%s world", str3));
+		
+		// split
+		String str4 = "java-string-split-method";
+		String[] words = str4.split("-");
+		// use .split(" ") to split on whitespace
+		for(String w : words) {
+			System.out.println(w);
+		}
+		
+		/*
+		 * note:
+		 * String API also contains other useful methods like
+		 * concat, contains, substring, trim, getChars
+		 * compareTo
+		 */
+		
+		// StringBuilder reverse method
+		String str5 = "erutaver";
+		StringBuilder sb = new StringBuilder(str5);
+		System.out.println(sb.reverse());
+		
+		// comparing StringBuilders
+		StringBuilder sb2 = new StringBuilder("revature");
+		System.out.println(sb.hashCode());
+		System.out.println(sb2.hashCode());
+		System.out.println(sb.equals(sb2));
+		
+		String str6 = "revature";
+		String str7 = "revature";
+		System.out.println(str6.hashCode());
+		System.out.println(str7.hashCode());
+		
+		// modify sb2's value
+		System.out.println(sb2.hashCode());
+		StringBuilder sb3 = sb2.append("!");
+		System.out.println(sb3.hashCode());
+		System.out.println(sb2.equals(sb3));
+	}
+
+	public static void funWithHashCodeAndEquals() {
+		String[] members1 = {"Calder", "Kiera", "Chad"}; 
+		Batch b1 = new Batch("1801Jan22", "Java", members1);
+		
+		System.out.println(b1.hashCode());
+		
+		String[] members2 = {"Matt", "Conan", "Sungkwon"}; 
+		Batch b2 = new Batch("1801Jan22", "Java", members2);
+		
+		System.out.println(b2.hashCode());
+		
+		System.out.println(b1.equals(b2));
+	}
+	public static void funWithArrays() {
+		// way to declare arrays
+		int[] intArray1 = {5, 6, 7};
+		int[] intArray2 = new int[7];
+		int intArray3[] = new int[7]; // legal but horrible
+		
+		System.out.println(intArray1.toString());
+		// use the Arrays utility class for useful methods
+		// also has a sort, search, etcetera...
+		System.out.println(Arrays.toString(intArray1));
+		
+		// not limited to one dimension
+		int[][] intArray4 = {{1,2,3}, {4, 5, 6}, {7, 8}};
+		int[][] intArray5 = new int[3][4];
+		System.out.println(intArray4.toString());
+		System.out.println(Arrays.toString(intArray4));
+		
+		for(int[] i : intArray4) { // (int i = 0; i < intArray4.length; i++) {
+			/*for(int j = 0; j < intArray4[i].length; j++) {
+				System.out.print(intArray4[i][j] + " ");
+			}*/
+			System.out.println(Arrays.toString(i));
+			System.out.println();
+		}
+		// throws ArrayIndexOutOfBounds exception
+		System.out.println(intArray1[5]);
+ 	}
+	
+	// varargs
+	// method will take a variable number of String arguments
+	// varargs must be the last argument
+	// can be any type
+	public static void funWithVarArgs(String ...a) {
+		// moving through the varargs
+		for(String s : a) {
+			System.out.println(s);
+		}
+	}
+}
