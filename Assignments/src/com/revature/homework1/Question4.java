3649c7b5482e9e251a3a1519c0390feaa97dc206
sungkwonkudo <30812602+sungkwonkudo@users.noreply.github.com>
2018-01-23 20:08:09 -0500

    completed Assignment 4

--- Assignments/src/com/revature/homework1/Question4.java
+++ Assignments/src/com/revature/homework1/Question4.java
@@ -1,5 +1,20 @@
 package com.revature.homework1;
 
+// Write a program to calculate N!
 public class Question4 {
-
+	public static void main(String[] args) {
+		
+		// Testing with a small number
+		System.out.println(factorial(0));
+		
+	}
+	
+	public static int factorial(int n) {
+		int answer=1;
+		for(int i=1; i<=n; i++) {
+			answer*=i;
+		}
+		
+		return answer;
+	}
 }
