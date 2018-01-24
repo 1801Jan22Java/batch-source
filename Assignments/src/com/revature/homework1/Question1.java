0ac4bf2abdac3aa9830dfbb98729171097e0249c
genome3po <jeffrey.rubi.885@my.csun.edu>
2018-01-23 18:34:05 -0800

    Created 'Assignments' project and started Q1

---
+++ Assignments/src/com/revature/homework1/Question1.java
@@ -0,0 +1,24 @@
+package com.revature.homework1;
+
+/* Created by: Jeffrey Rubi
+ * Date: 23 January 2018
+ * Perform a bubble sort on the following integer array:  1,0,5,6,3,2,3,7,9,8,4
+ */
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		// declared, initialized an int type array;
+		int[] testArray = {1,0,5,6,3,2,3,7,9,8,4};
+		
+	} // end main()
+	
+	public static int[] bubble(int[] toProcess) {
+		
+		// int type array for storing results
+		int[] result;
+		
+		return result;
+	} // end bubble()
+
+} // end class
