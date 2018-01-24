56f66940c96ab680322421eeecd0872b07f2215b
genome3po <jeffrey.rubi.885@my.csun.edu>
2018-01-23 20:07:20 -0800

    Completed Question1.java

--- Assignments/src/com/revature/homework1/Question1.java
+++ Assignments/src/com/revature/homework1/Question1.java
@@ -10,15 +10,34 @@ public class Question1 {
 	public static void main(String[] args) {
 		// declared, initialized an int type array;
 		int[] testArray = {1,0,5,6,3,2,3,7,9,8,4};
-		
+		bubble(testArray);
+		// print it.
+		for(int i = 0; i < testArray.length; i++) {
+			System.out.print(testArray[i] + " ");
+		} // end for
 	} // end main()
 	
-	public static int[] bubble(int[] toProcess) {
+	public static void bubble(int[] toProcess) {
+		/* Marker while array is not sorted, this will be 'true' until 'nextPass' is not 
+		 * needed. If 'nextPass' is not needed set to false.
+		 */
+		boolean nextPass = true;
 		
-		// int type array for storing results
-		int[] result;
-		
-		return result;
+		for(int j = 1; j < toProcess.length && nextPass; j++) {
+			// When array is sorted, set 'nextPass' to 'false'.
+			nextPass = false;
+			for(int i = 0; i < toProcess.length - j; i++) {
+				if(toProcess[i] > toProcess[i+1]) {
+					// Perform swap
+					int temp = toProcess[i];
+					toProcess[i] = toProcess[i+1];
+					toProcess[i+1] = temp;
+					
+					// Swap occurred so 'nextPass' still 'true'
+					nextPass = true;
+				} // end if
+			} // end for
+		} // end for
 	} // end bubble()
 
 } // end class
