e01dd086ac1de67d8036083ea80e5da4fd395e60
jaboyles <jacobboyles0115@gmail.com>
2018-01-24 16:58:34 -0500

    Finished Question 9

--- Assignments/src/com/revature/homework1/Question9.java
+++ Assignments/src/com/revature/homework1/Question9.java
@@ -13,21 +13,25 @@ public class Question9 {
 		ArrayList<Integer> prime = new ArrayList<>();
 		boolean add = false;
 		
+		//Load the first array list with numbers 1 through 100
 		for (int i = 1; i <= 100; i++) {
 			list.add(new Integer(i));
 		}
 		
 		
-		
 		for (Integer i : list) {
+			//need to add 1 to start the list
 			if (i == 1) {
 				prime.add(i);
-			}else if (i == 2) {
+			}else if (i == 2) {//need add two because 1 is a factor of everything
 				prime.add(i);
 			}else {
+				//we assume it is prime before we start checking
 				add = true;
-				for(Integer j : prime) {
-					if (j % i == 0) {
+				//we iterate through the current prime numbers to see if they are factors of the current number
+				for(int j = 1; j < prime.size(); j++) {
+					//if the current number is evenly divisible by he current prime number then it is not added
+					if (i % prime.get(j) == 0) {
 						add = false;
 						break;
 					}
