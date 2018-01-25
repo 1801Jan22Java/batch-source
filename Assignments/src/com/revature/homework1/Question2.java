c3656bc2c96f865a4988e25dae3f5942001ad10e
genome3po <jeffrey.rubi.885@my.csun.edu>
2018-01-24 17:35:01 -0800

    Completed Question2

--- Assignments/src/com/revature/homework1/Question2.java
+++ Assignments/src/com/revature/homework1/Question2.java
@@ -8,7 +8,24 @@ package com.revature.homework1;
 public class Question2 {
 
 	public static void main(String[] args) {
-
+		// no input from user embedded code, prints out the 1st 25 Fib. numbers
+		System.out.println("The first 25 Fibonacci number are:");
+		for(int i = 0; i < 25; i++) {
+			System.out.print(fib(i) + " ");
+		}
+	}
+	
+	public static long fib(long number) {
+		if(number == 0) return 0; // base case 1
+		else if(number == 1) return 1; // base case 2
+		// Fibonacci numbers are the sum of the 2 previous Fibs.
+		else return fib(number - 1) + fib(number - 2); 
 	}
 
 }
+
+/***checking
+ *  0 1 1 3 5 8 13....
+ *  0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 
+ */
+ 
