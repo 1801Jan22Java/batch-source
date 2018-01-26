cf1a0dc03c85fd80a5d0332d83e3319bc3938269
sungkwonkudo <30812602+sungkwonkudo@users.noreply.github.com>
2018-01-26 17:06:40 -0500

    Fixed some stuff

--- MavenDemo/src/main/java/com/revature/logging/App.java
+++ MavenDemo/src/main/java/com/revature/logging/App.java
@@ -5,25 +5,7 @@ public class App {
 	public static void main(String[] args) {
 		
 		System.out.println("hello world");
-<<<<<<< HEAD
-	
-		LoggingClass lc = new LoggingClass();
-		lc.allTheLogs();
-		
-		try {
-			divide(1, 0);
-			
-		} catch (Exception e) {
-			lc.giveFatal(e);
-		}
-		
-	}
-	
-	
-	public static int divide(int a, int b) {
-		return a/b;
-	}
-=======
+
 		
 		LoggingClass lc = new LoggingClass();
 		lc.allTheLogs();
@@ -39,6 +21,4 @@ public class App {
 	public static int divide(int a, int b){
 		return a/b;
 	}
-
->>>>>>> 39c64c4d101d1e56e70955d3bd4aa54d6f94e18e
 }
