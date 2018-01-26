ea841544be570bdc38cd0888cf5ad3abced4de7a
genome3po <jeffrey.rubi.885@my.csun.edu>
2018-01-25 19:59:19 -0500

    Updated 'com.revature.reflection' package 'Driver' class

--- HelloWorld/src/com/revature/reflection/Driver.java
+++ HelloWorld/src/com/revature/reflection/Driver.java
@@ -12,39 +12,50 @@ import com.revature.media.Movie;;
 public class Driver {
 
 	public static void main(String[] args) {
-		
+
 		List<Media> mediaList = new ArrayList<Media>();
 		Book b1 = new Book("Leo Tolstoy", "Anna Karenina", 1877, "fiction");
 		Book b2 = new Book("Fyodor Dostoyevsky", "Crime and Punishment", 1866, "fiction");
 		Movie m1 = new Movie("Pixar", "Toy Story", 1995, "cartoon");
 		Movie m2 = new Movie("Michael Chricton", "Jurassic Park", 1993, "science fiction");
 		Collections.addAll(mediaList, b1, b2, m1, m2);
+		System.out.println(genericReflections(mediaList));
+		Collections.shuffle(mediaList);
+		System.out.println(genericReflections(mediaList));
+	}
+	
+	public static <T> T genericReflections(List<T> l) {
+		for (T item : l) {
+			if (item.getClass().getName().equals("com.revature.media.Book")) {
+				return item;
+			} 
+		}
+		return null;
 	}
 
-	public static void funWithReflections {
-		
+	public static void funWithReflections() {
 		try {
-			
+
 			// get our class
 			Class clazz = Class.forName("com.revature.media.Media");
 			System.out.println(clazz.getSimpleName());
-			
+
 			// print fields
 			Field[] fields = clazz.getDeclaredFields();
-			for(Field f : fields) {
+			for (Field f : fields) {
 				System.out.println(f.getName() + " with a datatype of " + f.getType());
 			}
-			
+
 			// new instance of Book
 			Class clazz2 = Class.forName("com.revature.media.Book");
 			Book b1 = (Book) clazz2.newInstance();
 			System.out.println(b1);
 			Field publisher;
-			publisher = clazz2.getDeclaredField("publisher"); 
+			publisher = clazz2.getDeclaredField("publisher");
 			publisher.setAccessible(true);
 			publisher.set(b1, "Michael Bay");
 			System.out.println(b1);
-			
+
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (InstantiationException e) {
@@ -57,15 +68,5 @@ public class Driver {
 			e.printStackTrace();
 		}
 	}
-	
-	public static <T> T genericReflections(List<T> l) {
-		for(T item : l) {
-			if (item.getClass().getName().equals("com.revature.media.Book")) {
-				return item;
-			}
-		}
-	}
-	
-	
 
 }
