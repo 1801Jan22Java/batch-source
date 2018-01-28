2b4377eeeec873a7842ba22b43419c6f92973f11
Jahreal Symister <jahrealsymister@Jahreals-MacBook-Pro.local>
2018-01-28 17:10:47 -0500

    Assignment1

--- Assignments/src/com/revature/homework1/Question13.java
+++ Assignments/src/com/revature/homework1/Question13.java
@@ -3,8 +3,30 @@ package com.revature.homework1;
 public class Question13 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int lines = 0;
+		StringBuilder string = new StringBuilder();
+		boolean booleanVal = true;
+		
+		
+		while (lines != 4) {
+			while(booleanVal) {
+				string.append("0 ");
+				System.out.println(string.toString());
+				lines += 1;
+				booleanVal = false;
+			}
+			while(booleanVal == false) {
+				string.append("1 ");
+				System.out.println(string.toString());
+				lines += 1;
+				booleanVal = true;
+			}
+		}
+
+			
+		}
 
 	}
 
-}
+
