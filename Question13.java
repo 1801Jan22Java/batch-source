f9de3e374c411ec1b7b7602922c428c95351f498
mJohn142 <matthewJohnson29@outlook.com>
2018-01-28 10:43:43 -0800

    Question 11 for hwk#1

---
+++ Question13.java
@@ -0,0 +1,15 @@
+package com.revature.homework1;
+
+public class Question13 {
+
+	public static void main(String[] args) {
+		for(int i=0; i<6; i+=2) {
+			for(int j=0;j<i;j++);
+			{
+			System.out.println(0);
+		}
+			System.out.println(1);
+	}
+		System.out.println();
+}
+}
