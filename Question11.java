7d179388239fb538b50ba7ce721bac664d57fba1
mJohn142 <matthewJohnson29@outlook.com>
2018-01-28 10:46:21 -0800

    Question 11 hwk#1

---
+++ Question11.java
@@ -0,0 +1,22 @@
+package com.revature.homework11;
+
+public class Question11 {
+	private float number;
+	private float myNumber;
+	public float getNumber() {
+		return number;
+	}
+	public void setNumber(float number) {
+		this.number = number;
+	}
+	public float getMyNumber() {
+		return myNumber;
+	}
+	public void setMyNumber(float myNumber) {
+		this.myNumber = myNumber;
+	}
+	
+	
+	
+
+}
