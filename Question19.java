97b899c04874cfefaf50a972daab85e14bbac90f
mJohn142 <matthewJohnson29@outlook.com>
2018-01-28 10:51:04 -0800

    Question 19 hwk1

---
+++ Question19.java
@@ -0,0 +1,25 @@
+package com.revature.homework1;
+
+import java.util.ArrayList;
+
+public class Question19 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+		numbers.add(4);
+		numbers.add(5);
+		numbers.add(6);
+		numbers.add(7);
+		numbers.add(8);
+		numbers.add(9);
+		numbers.add(10);
+		
+		for(int i=0;i<numbers.size(); i++) {
+			System.out.println(i);
+		}
+	}
+}
