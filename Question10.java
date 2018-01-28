6dcf002a1b4863ea943ec71bbc5776fca51c132c
mJohn142 <matthewJohnson29@outlook.com>
2018-01-28 10:42:26 -0800

    Question 10 for hwk#1

---
+++ Question10.java
@@ -0,0 +1,14 @@
+package com.revature.homework1;
+
+public class Question10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n1 = 10;
+		int n2 = 8;
+		int result = (n1<n2) ? n1 : n2;
+		System.out.println(result);
+	}
+
+}
