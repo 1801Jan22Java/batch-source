1fca4fc2de5589cc395afde1bf761b26ca6d9c20
mJohn142 <matthewJohnson29@outlook.com>
2018-01-28 10:46:58 -0800

    Question11 tester

---
+++ Question11a.java
@@ -0,0 +1,18 @@
+package com.revature.homework11a;
+
+import com.revature.homework11.Question11;
+
+public class Question11a extends Question11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Question11a q1 = new Question11a();
+		q1.setNumber(10.23f);
+		Question11a q2 = new Question11a();
+		q2.setMyNumber(12.45f);
+		System.out.println(q1.getNumber());
+		System.out.println(q2.getMyNumber());
+	}
+
+}
