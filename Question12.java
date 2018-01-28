55da4551d3caa1886a533586e693b6cf9c5f6cad
mJohn142 <matthewJohnson29@outlook.com>
2018-01-28 10:44:56 -0800

    Question 12 for hwk#1

---
+++ Question12.java
@@ -0,0 +1,15 @@
+package com.revature.homework1;
+
+public class Question12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] num = {1, 50, 60, 70, 80, 90, 100};
+		for(int i : num) {
+			System.out.println(i);
+		}
+
+	}
+
+}
