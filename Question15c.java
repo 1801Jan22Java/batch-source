6272aea45e510d6405fab9544d8dec0ff4c44de6
mJohn142 <matthewJohnson29@outlook.com>
2018-01-28 10:49:30 -0800

    Question  15 tester hwk1

---
+++ Question15c.java
@@ -0,0 +1,16 @@
+package com.revature.homework1;
+
+public class Question15c implements Question15{
+ 
+	public static void main(String[] args) {
+		Question15c n1 = new Question15c();
+		Question15.addition(1,2);
+		Question15.subtraction(5,4);
+		Question15.multiplication(4, 5);
+		Question15.division(14, 7);
+		
+	}
+	
+
+	
+}
