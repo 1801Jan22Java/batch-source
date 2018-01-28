d4d53ccce7c56b904d8ac284bc09a32727ff9fd6
mJohn142 <matthewJohnson29@outlook.com>
2018-01-28 10:49:04 -0800

    Question 15 hwk1

---
+++ Question15.java
@@ -0,0 +1,20 @@
+package com.revature.homework1;
+
+public interface Question15 {
+
+	public static int addition(int a, int b) {
+		return a + b;
+	}
+	
+	public static int subtraction(int a, int b) {
+		return a-b;
+	}
+	
+	public static int multiplication(int a, int b) {
+		return a*b;
+	}
+	
+	public static int division(int a, int b) {
+		return a/b;
+	}
+}
