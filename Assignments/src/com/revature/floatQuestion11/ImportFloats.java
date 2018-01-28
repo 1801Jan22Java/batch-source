3323708aa77f15328267e4330389ac228a151570
Michael Chen <mchen092@gmail.com>
2018-01-28 18:31:43 -0500

    forgot to add other package for question 11

---
+++ Assignments/src/com/revature/floatQuestion11/ImportFloats.java
@@ -0,0 +1,10 @@
+package com.revature.floatQuestion11;
+
+public class ImportFloats {
+	public static final float f1 = 1.1f;
+	public static final float f2 = 2.2f;
+	
+	public static void main(String[] args) {
+		System.out.println("If you have coin, Khajit has floats :)");
+	}
+}
