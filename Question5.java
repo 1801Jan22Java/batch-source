7cc2b5c9c87289de1e56c281ef198c791b161e78
mJohn142 <matthewJohnson29@outlook.com>
2018-01-28 11:18:15 -0800

    question 5 hwk1

---
+++ Question5.java
@@ -0,0 +1,14 @@
+package com.revature.homework1;
+
+public class Question5 {
+
+	public static void main(String[] args) {
+		String str = new String("John went to school");
+		if(str.contains("school")) {
+			System.out.println("went to school");
+		}
+		else {
+			System.out.println("invalid");
+		}
+	}
+}
