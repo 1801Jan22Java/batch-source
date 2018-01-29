ee8302bffb5efc03f121c14b1a282e0248e49832
mJohn142 <matthewJohnson29@outlook.com>
2018-01-29 07:08:17 -0800

    Code Challenge Mutation class

---
+++ Driver.java
@@ -0,0 +1,12 @@
+package com.revature.codechallenge;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Mutation m1 = new Mutation("AACCGGTT", "A");
+		//Create a new object that would print out the string mutation.
+	}
+
+}
