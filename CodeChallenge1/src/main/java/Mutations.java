7ddbac8c90fbd8761267e5146d99eff7862cc727
jaboyles <jacobboyles0115@gmail.com>
2018-01-30 16:22:56 -0500

    formatting

--- CodeChallenge1/src/main/java/Mutations.java
+++ CodeChallenge1/src/main/java/Mutations.java
@@ -3,31 +3,29 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Mutations {
-	
+
 	public int mutation(Reader stream) {
-		
+
 		Scanner scan = new Scanner(stream);
-		
+
 		String start = scan.next();
-	
+
 		String end = scan.next();
-		
+
 		ArrayList<String> bank = new ArrayList<>();
 		StringBuilder currentMutation = new StringBuilder(start);
 		int numMutations = 0;
-		
+
 		if (start.equals(end)) {
 			return numMutations;
 		}
-		
-		
+
 		while (scan.hasNext()) {
 			bank.add(scan.next());
-			
+
 		}
 		scan.close();
-		
-		
+
 		if (bank.size() == 0 || !bank.contains(end)) {
 			return -1;
 		}
@@ -40,12 +38,12 @@ public class Mutations {
 				}
 			}
 		}
-		
+
 		if (!currentMutation.toString().equals(end)) {
 			return -1;
 		}
-		
+
 		return numMutations;
 	}
-	
+
 }
