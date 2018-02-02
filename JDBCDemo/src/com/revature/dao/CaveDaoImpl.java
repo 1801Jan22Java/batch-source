d03439f2c4437a09053a05f5ecfce5e6131d0d4f
erchiggins <emilyrchiggins@gmail.com>
2018-02-02 15:52:44 -0500

    all retrievals

--- JDBCDemo/src/com/revature/dao/CaveDaoImpl.java
+++ JDBCDemo/src/com/revature/dao/CaveDaoImpl.java
@@ -2,6 +2,7 @@ package com.revature.dao;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -12,22 +13,22 @@ import com.revature.beans.Cave;
 import com.revature.util.ConnectionUtil;
 
 public class CaveDaoImpl implements CaveDao {
-	
+
 	private static String filename = "connection.properties";
-	
-	public List<Cave> getCaves(){
+
+	public List<Cave> getCaves() {
 		List<Cave> cl = new ArrayList<>();
-		
-		try(Connection con = ConnectionUtil.getConnectionFromFile(filename)){
-			//using a Statement - beware SQL injection
+
+		try (Connection con = ConnectionUtil.getConnectionFromFile(filename)) {
+			// using a Statement - beware SQL injection
 			String sql = "SELECT * FROM CAVE";
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery(sql);
-			while(rs.next()){
+			while (rs.next()) {
 				int id = rs.getInt("CAVE_ID");
 				String name = rs.getString("CAVE_NAME");
 				int maxBears = rs.getInt("MAX_BEARS");
-				cl.add(new Cave(id,name,maxBears));
+				cl.add(new Cave(id, name, maxBears));
 			}
 			con.close();
 		} catch (SQLException e) {
@@ -35,14 +36,40 @@ public class CaveDaoImpl implements CaveDao {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return cl;
 	}
 
 	@Override
 	public Cave getCaveById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement pstmt = null;
+		Cave cave = null;
+		try (Connection con = ConnectionUtil.getConnectionFromFile(filename)) {
+			String sql = "SELECT * FROM CAVE WHERE CAVE_ID = ?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, id);
+			ResultSet rs = pstmt.executeQuery();
+			/*
+			while (rs.next()) {
+				System.out.println("in the while loop");
+				String name = rs.getString("CAVE_NAME");
+				int maxB = rs.getInt("MAX_BEARS");
+				cave = new Cave(id, name, maxB);
+			}
+			*/
+			//alternate approach
+			if(rs.next()){
+				String name = rs.getString("CAVE_NAME");
+				int maxB = rs.getInt("MAX_BEARS");
+				cave = new Cave(id, name, maxB);
+			}
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return cave;
 	}
 
 }
--- JDBCDemo/src/com/revature/main/Driver.java
+++ JDBCDemo/src/com/revature/main/Driver.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import com.revature.dao.BearDao;
+import com.revature.dao.BearDaoImpl;
 import com.revature.dao.CaveDao;
 import com.revature.dao.CaveDaoImpl;
 import com.revature.util.ConnectionUtil;
@@ -26,7 +28,11 @@ public class Driver {
 		*/
 		
 		CaveDao cd = new CaveDaoImpl();
-		System.out.println(cd.getCaves().toString());
+		//System.out.println(cd.getCaves().toString());
+		System.out.println(cd.getCaveById(10));
+		
+		BearDao bd = new BearDaoImpl();
+		System.out.println(bd.getBears().toString());
 	}
 
 }
