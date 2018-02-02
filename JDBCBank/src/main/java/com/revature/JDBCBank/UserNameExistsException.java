0d8583df0cbdf144b249cef58cfdd84cd2db7048
sungkwonkudo <30812602+sungkwonkudo@users.noreply.github.com>
2018-02-02 09:50:49 -0500

    Created custom exception

--- JDBCBank/src/main/java/com/revature/JDBCBank/UserNameExistsException.java
+++ JDBCBank/src/main/java/com/revature/JDBCBank/UserNameExistsException.java
@@ -1,5 +1,41 @@
 package com.revature.JDBCBank;
 
-public class UserNameExistsException {
+public class UserNameExistsException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3129498241007695884L;
+
+	private String name;
+
+	public UserNameExistsException(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public UserNameExistsException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserNameExistsException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserNameExistsException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserNameExistsException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
 
 }
