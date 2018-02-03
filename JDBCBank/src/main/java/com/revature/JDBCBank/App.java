1a105bc08f846568cea550d5ba1a24e8cad55dd5
Michael Chen <mchen092@gmail.com>
2018-02-03 16:23:17 -0500

    initial commit JDBCBank

---
+++ JDBCBank/src/main/java/com/revature/JDBCBank/App.java
@@ -0,0 +1,13 @@
+package com.revature.JDBCBank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
