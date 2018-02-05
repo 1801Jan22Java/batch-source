da1014de9065e7baa983ed04a4b74073b4d63f4c
sungkwonkudo <30812602+sungkwonkudo@users.noreply.github.com>
2018-02-05 16:47:14 -0500

    Further component testing of the gui

--- JDBCBank/src/test/java/com/revature/JDBCBank/JDBCApplicationTest.java
+++ JDBCBank/src/test/java/com/revature/JDBCBank/JDBCApplicationTest.java
@@ -1,10 +1,80 @@
 package com.revature.JDBCBank;
 
-public class JDBCApplicationTest {
+import static org.junit.Assert.assertTrue;
 
-	public static void main(String[] args) {
-		// General starting GUI
-		
+import org.junit.Test;
+
+public class JDBCApplicationTest {
+	
+	//Test valid input for integer and double
+	@Test
+	public void validAccountAccess() {
+		boolean test = false;
+		try {
+			String input = "1";
+			String testvar = "not a number text";
+			
+			switch (input) {
+
+			case "1":
+				System.out.println("Choose account number to withdraw from: ");
+				Integer wAccount = Integer.parseInt(testvar);
+				break;
+
+			}
+		} catch (NumberFormatException e) {
+			System.out.println("Invalid input");
+			test=true;
+		}
+		assertTrue(test);
+	}
+
+	// Testing for ability to catch NumberFormatException for 
+	// parseInt and parseDouble within the try catch.
+	@Test
+	public void invalidNonIntegerInputAccountAccess() {
+		boolean test = true;
+		try {
+			String input = "1";
+			String testvar = "5";
+			
+			switch (input) {
+
+			case "1":
+				System.out.println("Choose account number to withdraw from: ");
+				Integer wAccount = Integer.parseInt(testvar);
+				Double iAccount = Double.parseDouble(testvar);
+				break;
+
+			}
+		} catch (NumberFormatException e) {
+			System.out.println("Invalid input");
+			test=false;
+		}
+		assertTrue(test);
+	}
+	
+	// Test invalid double input
+	@Test
+	public void invalidNonDoubleInputAccountAccess() {
+		boolean test = false;
+		try {
+			String input = "1";
+			String testvar = "not a number text";
+			
+			switch (input) {
+
+			case "1":
+				System.out.println("Choose account number to withdraw from: ");
+				Double wAccount = Double.parseDouble(testvar);
+				break;
+
+			}
+		} catch (NumberFormatException e) {
+			System.out.println("Invalid input");
+			test=true;
+		}
+		assertTrue(test);
 	}
 	
 	
