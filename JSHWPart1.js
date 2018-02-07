8c7c56bdc9ab750327c8cb526347b524aaf84a13
sungkwonkudo <30812602+sungkwonkudo@users.noreply.github.com>
2018-02-07 16:23:11 -0500

    changed pom.xml, added javascript assignment

---
+++ JSHWPart1.js
@@ -0,0 +1,70 @@
+/*
+ 1. Return the nth fibonacci number
+
+ f(0) = 0
+ f(1) = 1
+ f(10) = 55
+*/
+homework.fibonacci = function(n){
+
+};
+
+/*
+ 2. Sort array of integers
+
+ f([2,4,5,1,3,1]) = [1,1,2,3,4,5]
+
+ Don't use the Array sort() method... that would be lame.
+*/
+homework.sort = function(array) {
+
+};
+
+/*
+ 3. Return the factorial of n
+
+ f(0) = 1
+ f(1) = 1
+ f(3) = 6
+*/
+homework.factorial = function(n){
+
+};
+
+/*
+ 4. Rotate left
+
+ Given array, rotate left n times and return array
+
+ f([1,2,3,4,5], 1) = [2,3,4,5,1]
+ f([1,2,3,4,5], 6) = [2,3,4,5,1]
+ f([1,2,3,4,5], 3) = [4,5,1,2,3]
+
+*/
+homework.rotateLeft = function(array, n) {
+
+};
+
+/*
+ 5. Balanced Brackets
+
+ A bracket is any one of the following: (, ), {, }, [, or ]
+
+ The following are balanced brackets:
+    ()
+    ()()
+    (())
+    ({[]})
+
+ The following are NOT balanced brackets:
+ (
+ )
+ (()
+ ([)]
+
+ Return true if balanced
+ Return false if not balanced
+*/
+homework.balancedBrackets = function(bracketsString){
+
+};
