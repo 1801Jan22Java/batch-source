38fdc684ff8e7f70d2a5c443f911cbec936813b4
jaboyles <jacobboyles0115@gmail.com>
2018-02-08 12:46:27 -0500

    Added Java Script Demo

---
+++ JavaScriptDemo/src/main/webapp/scripts/closure.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+window.onload = function(){
+	counter = getCount();
+	appleCounter = getCount("apple");
+	orangeCounter = getCount("orange");
+}
+
+function getCount(noun) {
+	var count = 0;
+	return function() {
+		count += 1;
+		console.log(noun + " count is " + count);
+		return count;
+	}
+}
---
+++ JavaScriptDemo/src/main/webapp/scripts/parse.js
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+
+
+var bearText = '{"bears" : [{"name" : "Ferdinand", "cave" : {"id" : 3, "name" : "Reston"}}, {"name" : "Bear2", "cave" : {"id" : 4, "name" : "Sacramento"}}]}'
+	
+var bearObj = JSON.parse(bearText);
+
+window.onload = function(){
+	console.log(bearObj);
+}
+
+function showBears() {
+	var bearDiv = document.getElementById("bearDiv");
+	var bears = bearObj.bears;
+	for (var i = 0; i < bears.length; i++){
+		console.log(bears[i]);
+		var newDiv = document.createElement("div");
+		newDiv.setAttribute("class", "displayDiv");
+		newDiv.innerHTML = "name : " + bears[i].name +"; cave: " + bears[i].cave.name;
+		bearDiv.appendChild(newDiv);
+	}
+}
