0eba5ef112d2e6df057370e05b028c25a3d111dc
nrhassan <35673818+nrhassan@users.noreply.github.com>
2018-02-12 01:13:08 -0500

    added more to hw2

--- JSHW2.js
+++ JSHW2.js
@@ -13,7 +13,7 @@ Due 5pm, Monday, February 12 in your branch.
 
 -----------------------------------------------------------------------------------
 */
-var homework = {};
+
 /*1. USA
 Define function getUSA()
 
@@ -21,7 +21,7 @@ Find the html element that contains "USA".
 
 Print that element's contents.*/
 
-homework.getUSA = function(){
+function getUSA(){
     var elem = document.querySelector("span[data-customAttr=USA]");
     console.log(elem);
     
@@ -34,15 +34,15 @@ homework.getUSA = function(){
 Define function getPeopleInSales()
 
 Print the names of all the people in the sales department.*/
-homework.getPeopleInSales = function(){
+function getPeopleInSales(){
     var emplist = document.getElementsByClassName("empName");
     var i ;
     for(i=0; i<emplist.length;i++){
-        console.log(emplist.item(i));
-        console.log(emplist.length(i).element.nextElementSibling)
+        if (emplist[i].nextElementSibling.textContent=="Sales"){
+            console.log(emplist[i]);
+        }
     }
 }
-
 
 /*
 3. Click Here
@@ -53,15 +53,34 @@ Find all anchor elements with a <span> child.
 
 Print the contents of <span>
 */  
-
+function getAnchorChildren(){
+    var anchor = document.getElementsByTagName("a");
+    for(var i=0; i<anchor.length; i++){
+        var sp = anchor[i].getElementsByTagName("span");
+        for(var j=0; j<sp.length; j++){
+           console.log(sp[j].textContent);
+       } 
+    }
+}
 
 /*
 4. Hobbies
-Define function 	
+Define function getSkills()
+
 Find all checked options in the 'skills' select element.
 
-Print the value and the contents.
+Print the value and the contents
 */
+
+function getSkills(){
+    var skill = document.getElementsByName("skills");
+    for (var i=0; i<skill.length; i++){
+        var children = skill[i].childNodes;
+        for(var j=0; j<children.length; j++){
+            console.log(children[j].textContent)
+        }
+    }
+}
 
 
 /*
@@ -75,9 +94,15 @@ Print the value of the attribute.
 
 Print the element that has the attribute.
 */
+function getCustomAttribute(){
+    var custom = document.querySelectorAll("data-customAttr");
+    for(var i=0; i<custom.length;i++){
+        console.log(custom[i]);
+    }
+}
 
 
-
+/*
 6. Sum Event
 
 NOTE: Write unobtrusive Javascript
@@ -98,9 +123,25 @@ Add <input> element values.
 Put the sum in the <span> element.
 
 If values cannot be added, put "Cannot add" in the <span> element
+*/
+var a = document.getElementById("num1");
+var b = document.getElementById("num2");
+function addEvent() {
+    var val1 = a.value;
+    var val2 = b.value;
+    var answer = (parseInt(val1) + parseInt(val2))
+    var sum = document.getElementById("sum");
+    if (!isNaN(answer)) {
+        sum.textContent = answer;
+    } else {
+        sum.textContent = "Cannot add";
+    }
+}
+num1.addEventListener("change", addEvent, false);
+num2.addEventListener("change", addEvent, false);
 
 
-7. Skills Event
+/*7. Skills Event
 
 NOTE: Write unobtrusive Javascript
 
@@ -108,9 +149,21 @@ When user selects a skill, create an alert with a message similar to:
 	
 "Are you sure CSS is one of your skills?"
 
-NOTE: no alert should appear when user deselects a skill.
+NOTE: no alert should appear when user deselects a skill.*/
+    var skillList = document.getElementsByTagName("select");
+    var skill;
+    for (var i = 0; i < skillList.length; i++) {
+        if (skillList[i].name == "skills") {
+            skill = skillList[i];
+        }
+    }
+    skill.addEventListener("change", function () {
+        var text = skill.options[skill.selectedIndex].text;
+        alert("Are you sure " + text + " is one of your skills?");
+    })
 
 
+/*
 8. Favorite Color Event
 
 NOTE: Write unobtrusive Javascript
@@ -125,18 +178,54 @@ In this example, green is the new value and blue is the old value.
 
 Make the background color (of all favoriteColor radio buttons) 
 the newly selected favoriteColor
+*/
+
+document.getElementsByName("favoriteColor").forEach(function(elem) {
+    elem.addEventListener("change",backgroundColorChange);
+});
+
+function backgroundColorChange(event) {
+    var prev = document.body.style.backgroundColor;
+    if (prev == "") {
+        prev = "white";
+    }
+    console.log(event.target);
+    document.body.style.backgroundColor = event.target.value;
+    alert("So you like " + event.target.value + " more than " + prev + " now?");
+	
+}
 
 
-9. Show/Hide Event
+/*9. Show/Hide Event
 
 NOTE: Write unobtrusive Javascript
 
 When user hovers over an employees name:
 	
 Hide the name if shown.
-	Show the name if hidden.
+	Show the name if hidden.*/
+
+empList = document.getElementsByClassName("empName");
+
+for (var i = 0; i < document.getElementsByClassName("empName").length; i++)
+document.getElementsByClassName("empName")[i].addEventListener("mouseover",hideEmployeeName);
+
+function hideEmployeeName(event){
+    console.log(event.target);
+    var e = event.target;
+    if (event.target.style.opacity ==1)
+    {
+        event.target.style.opacity = 0;
+    }
+    else
+    {
+        event.target.style.opacity = 1;
+    }
+    console.log(e.style.display);
+};
 
 
+/*
 10. Current Time
 
 Regarding this element:
@@ -145,14 +234,46 @@ Regarding this element:
 Show the current time in this element in this format: 9:05:23 AM
 
 The time should be accurate to the second without having to reload the page.
+*/
+function displayCurrentTime() {
+    var date = new Date();
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    var seconds = date.getSeconds();
+    var zeroSec;
+    if (seconds < 10) {
+        zeroSec = "0" + seconds;
+    } else {
+        zeroSec = seconds;
+    }
+    var zeroMin
+    if (minute < 10) {
+        zeroMin = "0" + minute;
+    } else {
+        zeroMin = minute;
+    }
+    var regTime;
+    var ampm;
+    if (hour > 12) {
+        regTime = hour % 12;
+        ampm = "PM"
+    } else {
+        regTime = hour;
+        ampm = "AM"
+    }
+    document.getElementById("currentTime").innerHTML = regTime + ":" + zeroMin + ":" + zeroSec + " " + ampm;
+}
+setInterval(displayCurrentTime, 1000);
 
 
-11. Delay
+/*11. Delay
 Regarding this element:
 	
 <p id="helloWorld">Hello, World!</p>
 
-Three seconds after a user clicks on this element, change the text to a random color.
+Three seconds after a user clicks on this element, change the text to a random color.*/
+
+/*
 12. Walk the DOM
 
 Define function walkTheDOM(node, func)
@@ -161,5 +282,15 @@ This function should traverse every node in the DOM.
 Use recursion.
 
 On each node, call func(node).
+*/
+function walkTheDOM(node, func) {
+    func(node);
+    node = node.firstChild;
+    while (node) {
+        walkTheDOM(node, func);
+        node = node.nextSibling;
+    }
+}
+
 
 
