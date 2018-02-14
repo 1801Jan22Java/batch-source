ee92fb96f8f2df5002513c71cdfa1675d93db2db
erchiggins <emilyrchiggins@gmail.com>
2018-02-14 15:14:24 -0500

    wednesday afternoon part 2

---
+++ FrontControllerDemo/src/main/java/com/revature/servlet/MasterServlet.java
@@ -0,0 +1,43 @@
+package com.revature.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.revature.util.RequestHelper;
+
+/**
+ * Servlet implementation class MasterServlet
+ */
+public class MasterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MasterServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("views/Index.html");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String destination = RequestHelper.process(request);
+		response.sendRedirect(destination);
+	}
+
+}
