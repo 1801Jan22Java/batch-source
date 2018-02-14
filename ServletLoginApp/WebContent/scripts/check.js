fbe966cc3dccef592ad85d9bee85e935a58499f6
jaboyles <jacobboyles0115@gmail.com>
2018-02-14 10:39:50 -0500

    Added Session Management demo through login and profile

---
+++ ServletLoginApp/WebContent/scripts/check.js
@@ -0,0 +1,36 @@
+
+
+function sendAjaxGet(url, func) {
+	//step 1: obtain XHR
+	var xhr = new XMLHttpRequest() || new ActiveXObject("Microsoft.HTTPRequest");
+	//step 2: define onreadystatechange
+	xhr.onreadystatechange = function(){
+		console.log(this.readyState);
+		if (this.readyState == 4 && this.status == 200) {
+			func(this);
+		}
+		
+	}
+	//step 3: prepare request
+	
+	xhr.open("GET",url,true);
+	
+	//step 4: send request
+	
+	xhr.send();
+	
+};
+
+function populateUser(xhr) {
+	var res = JSON.parse(xhr.responseText);
+	if (res.username != "null") {
+		document.getElementById("user").innerHTML = "you are logged in as " + res.username;
+		
+	}else {
+		window.location = "http://localhost:8084/ServletLoginApp/login";
+	}
+};
+
+window.onload = function() {
+	sendAjaxGet("http://localhost:8084/ServletLoginApp/session", populateUser)
+}
---
+++ ServletLoginApp/src/com/revature/servlet/ProfileServlet.java
@@ -0,0 +1,26 @@
+package com.revature.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class ProfileServlet extends HttpServlet{
+
+	private static final long serialVersionUID = -2625346607120644446L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession(false);
+		
+		if (session != null && session.getAttribute("username") != null) {
+			req.getRequestDispatcher("Profile.html").forward(req, resp);
+		}else {
+			resp.sendRedirect("login");
+		}
+		
+	}
+}
