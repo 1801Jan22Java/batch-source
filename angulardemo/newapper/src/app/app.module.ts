d43068508652137f42796b06288dcfef7efa882b
Omar Mohamad <omarbmohamad@gmail.com>
2018-02-16 11:26:46 -0500

    Friday Angular Demo
    Work form angular demo Friday morning

--- angulardemo/newapper/src/app/app.module.ts
+++ angulardemo/newapper/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DatabindingComponent } from './components/databinding/databinding.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatabindingComponent
   ],
   imports: [
     BrowserModule
---
+++ angulardemo/newapper/src/app/components/databinding/databinding.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-databinding',
+  templateUrl: './databinding.component.html',
+  styleUrls: ['./databinding.component.css']
+})
+export class DatabindingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
