fb91a777e39b6cef777701f96e7c09b91be41f15
sungkwonkudo <30812602+sungkwonkudo@users.noreply.github.com>
2018-02-22 16:28:32 -0500

    final commit?

--- Project1/src/main/webapp/scripts/employeeProfileAjax.js
+++ Project1/src/main/webapp/scripts/employeeProfileAjax.js
@@ -1,9 +1,6 @@
 function sendAjaxGet(url, func) {
-	// Step 1: obtain xhr
 	var xhr = new XMLHttpRequest() || new AciveXObject("Microsoft.HTTPRequest");
 
-	// Step 2: define onreadystatechange
-	// We want to check the ready state and the status
 	xhr.onreadystatechange = function() {
 		console.log(this.readyState);
 		if (this.readyState == 4 && this.status == 200) {
@@ -11,7 +8,6 @@ function sendAjaxGet(url, func) {
 		}
 	}
 
-	// Step 3: prepare request
 	xhr.open("GET", url, true);
 	xhr.send();
 
--- Project1/src/main/webapp/scripts/profileAjax.js
+++ Project1/src/main/webapp/scripts/profileAjax.js
@@ -3,18 +3,14 @@
  */
 
 function sendAjaxGet(url, func) {
-	// Step 1: obtain xhr
 	var xhr = new XMLHttpRequest() || new AciveXObject("Microsoft.HTTPRequest");
 
-	// Step 2: define onreadystatechange
-	// We want to check the ready state and the status
 	xhr.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			func(this)
 		}
 	}
 
-	// Step 3: prepare request
 	xhr.open("GET", url, true);
 	xhr.send();
 
