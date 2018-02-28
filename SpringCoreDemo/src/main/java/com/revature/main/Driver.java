5b225c66cb0cb76c5e473e0ff99e7a1d83715181
erchiggins <emilyrchiggins@gmail.com>
2018-02-28 11:58:59 -0500

    Wednesday morning

--- SpringCoreDemo/src/main/java/com/revature/main/Driver.java
+++ SpringCoreDemo/src/main/java/com/revature/main/Driver.java
@@ -4,7 +4,10 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.revature.beans.AutoBearByName;
+import com.revature.beans.AutoBearByType;
 import com.revature.beans.Bear;
+import com.revature.beans.BearWithAuto;
 import com.revature.beans.BearWithConstructor;
 import com.revature.beans.BearWithSetter;
 import com.revature.beans.Cave;
@@ -13,20 +16,26 @@ public class Driver {
 
 	public static void main(String[] args) {
 		ApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
-		
-		//funWithCaves(ac);
-		
+
+		// funWithCaves(ac);
+
 		funWithBears(ac);
-		
+
 		// close AC after you're done with it
 		((AbstractApplicationContext) ac).close();
 	}
-	
+
 	static void funWithBears(ApplicationContext ac) {
 		Bear b1 = (BearWithSetter) ac.getBean("bearWithSetter");
 		b1.methodInBear();
 		Bear b2 = (BearWithConstructor) ac.getBean("bearWithConstructor");
 		b2.methodInBear();
+		Bear b3 = (AutoBearByName) ac.getBean("autoBearByName");
+		b3.methodInBear();
+		Bear b4 = (AutoBearByType) ac.getBean("autoBearByType");
+		b4.methodInBear();
+		Bear b5 = (BearWithAuto) ac.getBean("bearWithAuto");
+		b5.methodInBear();
 	}
 
 	static void funWithCaves(ApplicationContext ac) {
