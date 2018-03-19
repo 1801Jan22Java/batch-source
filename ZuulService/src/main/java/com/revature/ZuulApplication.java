10caf45e5a27aa4c815f5b8827a235e9ff0519bb
jcunneen <jcunneen@cs.stonybrook.edu>
2018-03-19 12:09:20 -0400

    zuul is here

---
+++ ZuulService/src/main/java/com/revature/ZuulApplication.java
@@ -0,0 +1,22 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.revature.filters.SimplePreFilter;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+	
+	@Bean
+	public SimplePreFilter simplePreFilter() {
+		return new SimplePreFilter();
+	}
+}
---
+++ ZuulService/src/main/java/com/revature/filters/SimplePreFilter.java
@@ -0,0 +1,30 @@
+package com.revature.filters;
+
+import com.netflix.zuul.ZuulFilter;
+
+public class SimplePreFilter extends ZuulFilter {
+
+	@Override
+	public Object run() {
+		System.out.println("hello from Zuul filter");
+		return null;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+	
+	
+
+}
